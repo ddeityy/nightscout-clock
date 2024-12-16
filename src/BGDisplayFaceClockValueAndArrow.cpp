386b9b6e36402950846733f2a38e252352d8108e
ddeityy <ddeityy@gmail.com>
2024-12-16 20:23:38 +0300

    test deploy

--- src/BGDisplayFaceClockValueAndArrow.cpp
+++ src/BGDisplayFaceClockValueAndArrow.cpp
@@ -10,6 +10,5 @@ void BGDisplayFaceClockValueAndArrow::showReadings(const std::list<GlucoseReadin
 
     showReading(readings.back(), 0, 7, TEXT_ALIGNMENT::CENTER, FONT_TYPE::MEDIUM, dataIsOld);
 
-    // show arrow in the right part of the screen
     showTrendArrow(readings.back(), 32 - 5, 1);
 }
